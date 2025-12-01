952e926304e2c0df07ae29b2aa94180b6d220070
Ahnaf Shahriar <unknownhacker400@gmail.com>
2025-12-01 22:22:46 +0600

    Vector with class implementation.cpp

--- Practice prob 1 Employee.cpp
+++ Practice prob 1 Employee.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 
 using namespace std;
 
@@ -8,88 +9,86 @@ class Employee{
     string name;
     double salary;
 
-public:
+    public:
     int id;
-    Employee(string n,int id,double s):name(n),id(id),salary(s){
 
+    Employee(string n,double s,int i){
+        name=n;
+        salary=s;
+        id=i;
     }
+
+    void display(){
+        cout<<"Name:    "<<name<<" ID:  "<<id<<" Salary:    "<<salary<<endl;
+    }
+
     double getsalary(){
         return salary;
     }
 
-    void display(){
-    cout<<"Name: "<<name<<" ID:"<<id<<" Salary:"<<salary<<endl;
+    void Increment(double percentage){
+        salary+=salary*percentage/100;
     }
 
-    void Increment(double percentage){
-    salary += (salary * percentage)/100;
-}
 
 };
-void print(vector <Employee> emp){
+
+void print(vector<Employee> emp){
     for(Employee e:emp){
         e.display();
     }
 }
 
-bool compareid(Employee e1,Employee e2){
+bool compareid(Employee e1, Employee e2){
     return e1.id<e2.id;
 }
 
-bool comparesalary(Employee e1,Employee e2){
-    return e1.id>e2.id;
+bool comparesal(Employee e1, Employee e2){
+    return e1.getsalary()>e2.getsalary();
 }
 
-int main(){
+int main() {
 
-vector <Employee> emp={ Employee("Rahim", 2, 8000),
-                                Employee("Karim", 1, 15000),
-                                Employee("Abir",4,9000),
-                                Employee("Roni", 3, 18000),
-};
-print(emp);
-
-cout<<endl<<"---Add an additional CEO---";
-cout<<"Name: ";
-string n;
-cin>>n;
-
-Employee e1(n,5,120000);
-emp.push_back(e1);
+  vector <Employee> emp={Employee("Rahim",8000,2),
+                        Employee("Karim",15000,1),
+                        Employee("Abir",9000,4),
+                        Employee("Roni",18000,3),
+  };
 
 print(emp);
 
-cout<<endl<<"Empoyees with salary more than 10000: "<<endl;
+cout<<"Employees with salary more than 10000$ :   "<<endl<<endl<<endl;
+
 for(Employee e:emp){
     if(e.getsalary()>=10000){
         e.display();
     }
 }
 
-cout<<"These employees' salary has been increased:"<<endl;
+cout<<"Employees have got increase in salary by 15% and 10% :   "<<endl<<endl<<endl;
 
 for(Employee &e:emp){
     if(e.getsalary()>=15000){
         e.Increment(15);
-    }else{
-         e.Increment(10);
+    }
+    else{
+        e.Increment(10);
     }
 }
 
-cout<<endl<<"After salary increment:"<<endl;
 print(emp);
 
-cout<<endl<<"---Sorted the employees by id (ascending)---"<<endl;
 sort(emp.begin(),emp.end(),compareid);
 
+cout<<"Employees sorted by ID :   "<<endl<<endl<<endl;
 print(emp);
 
-cout<<endl<<"---Sorted the employees by salary (descending)---"<<endl;
-sort(emp.begin(),emp.end(),comparesalary);
+sort(emp.begin(),emp.end(),comparesal);
 
+cout<<"Employees sorted by salary :   "<<endl<<endl<<endl;
 print(emp);
 
-cout<<endl<<"---*Employee with the highest salary*---"<<endl;
+cout<<"Employee with highest salary:    ";
 emp[0].display();
 
 }
